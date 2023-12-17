6a6794fc66e4d9ebe3e3b4ddcd00114ef6c77239
Karan Brar <neoatom@Karans-MacBook-Pro.local>
2023-12-16 18:31:52 -0800

    implement -c option of wc

---
+++ cmd/root.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var countBytes bool
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:  "ccwc [file]",
+	Args: cobra.MinimumNArgs(1),
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fileName := args[0]
+		if countBytes {
+			fileInfo, err := os.Stat(fileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%d %s\n", fileInfo.Size(), fileName)
+		}
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ccwc.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&countBytes, "count", "c", false, "Count the number of bytes in the file")
+}

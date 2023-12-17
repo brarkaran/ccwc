f9cb8f3aa3a86385aab009472120dfb7df4625c2
Karan Brar <neoatom@Karans-MacBook-Pro.local>
2023-12-16 18:49:28 -0800

    implement options -l, -w, and -m

--- cmd/root.go
+++ cmd/root.go
@@ -4,14 +4,19 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 var countBytes bool
+var countLines bool
+var countWords bool
+var countChars bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -27,6 +32,48 @@ var rootCmd = &cobra.Command{
 				log.Fatal(err)
 			}
 			fmt.Printf("%d %s\n", fileInfo.Size(), fileName)
+		}
+		if countLines {
+			file, err := os.Open(fileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+			fileScanner := bufio.NewScanner(file)
+			lineCount := 0
+			for fileScanner.Scan() {
+				lineCount++
+			}
+			fmt.Printf("%d %s\n", lineCount, fileName)
+		}
+		if countWords {
+			file, err := os.Open(fileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+			fileScanner := bufio.NewScanner(file)
+			wordCount := 0
+			for fileScanner.Scan() {
+				line := fileScanner.Text()
+				words := strings.Fields(line)
+				wordCount += len(words)
+			}
+			fmt.Printf("%d %s\n", wordCount, fileName)
+		}
+		if countChars {
+			file, err := os.Open(fileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+			fileScanner := bufio.NewScanner(file)
+			fileScanner.Split(bufio.ScanRunes)
+			charCount := 0
+			for fileScanner.Scan() {
+				charCount++
+			}
+			fmt.Printf("%d %s\n", charCount, fileName)
 		}
 	},
 }
@@ -50,5 +97,8 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.Flags().BoolVarP(&countBytes, "count", "c", false, "Count the number of bytes in the file")
+	rootCmd.Flags().BoolVarP(&countBytes, "bytes", "c", false, "Count the number of bytes in the file")
+	rootCmd.Flags().BoolVarP(&countLines, "lines", "l", false, "Count the number of lines in the file")
+	rootCmd.Flags().BoolVarP(&countWords, "words", "w", false, "Count the number of words in the file")
+	rootCmd.Flags().BoolVarP(&countChars, "characters", "m", false, "Count the number of characters in the file")
 }

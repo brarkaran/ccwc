b020bb213e014d8f6696285f93c76a43d22a0a96
Karan Brar <neoatom@neocore.local>
2023-12-19 14:56:46 -0800

    Major refactor of code

--- cmd/root.go
+++ cmd/root.go
@@ -6,75 +6,107 @@ package cmd
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"unicode"
 
 	"github.com/spf13/cobra"
 )
 
-var countBytes bool
-var countLines bool
-var countWords bool
-var countChars bool
+type cliOptions struct {
+	bytes bool
+	lines bool
+	words bool
+	chars bool
+}
+
+type fileStats struct {
+	bytes uint64
+	lines uint64
+	words uint64
+	chars uint64
+}
+
+var options cliOptions
+
+func formatStats(options *cliOptions, stats fileStats) string {
+	var result []string
+	if options.bytes {
+		result = append(result, strconv.FormatUint(stats.bytes, 10))
+	}
+	if options.lines {
+		result = append(result, strconv.FormatUint(stats.lines, 10))
+	}
+	if options.words {
+		result = append(result, strconv.FormatUint(stats.words, 10))
+	}
+	if options.chars {
+		result = append(result, strconv.FormatUint(stats.chars, 10))
+	}
+	return strings.Join(result, " ")
+}
+
+func computeStats(buf *bufio.Reader) fileStats {
+	var prevChar rune
+	var bytesCount uint64
+	var linesCount uint64
+	var wordsCount uint64
+	var charsCount uint64
+
+	for {
+		char, bytes, err := buf.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				if prevChar != rune(0) && !unicode.IsSpace(prevChar) {
+					wordsCount++
+				}
+				break
+			}
+			log.Fatal(err)
+		}
+		bytesCount += uint64(bytes)
+		charsCount++
+		if char == '\n' {
+			linesCount++
+		}
+		if !unicode.IsSpace(prevChar) && unicode.IsSpace(char) {
+			wordsCount++
+		}
+		prevChar = char
+	}
+	return fileStats{bytes: bytesCount, lines: linesCount, words: wordsCount, chars: charsCount}
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:  "ccwc [file]",
-	Args: cobra.MinimumNArgs(1),
+	Args: cobra.MinimumNArgs(0),
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-		fileName := args[0]
-		if countBytes {
-			fileInfo, err := os.Stat(fileName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("%d %s\n", fileInfo.Size(), fileName)
+		if !options.bytes && !options.chars && !options.words && !options.lines {
+			options.bytes = true
+			options.chars = true
+			options.words = true
+			options.lines = true
 		}
-		if countLines {
-			file, err := os.Open(fileName)
-			if err != nil {
-				log.Fatal(err)
+		if len(args) == 0 {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Printf("%s\n", formatStats(&options, computeStats(reader)))
+		} else {
+			for _, filename := range args {
+				file, err := os.Open(filename)
+				if err != nil {
+					log.Fatal(err)
+				}
+				reader := bufio.NewReader(file)
+				fmt.Printf("%s %s\n", formatStats(&options, computeStats(reader)), filename)
 			}
-			defer file.Close()
-			fileScanner := bufio.NewScanner(file)
-			lineCount := 0
-			for fileScanner.Scan() {
-				lineCount++
-			}
-			fmt.Printf("%d %s\n", lineCount, fileName)
 		}
-		if countWords {
-			file, err := os.Open(fileName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
-			fileScanner := bufio.NewScanner(file)
-			wordCount := 0
-			for fileScanner.Scan() {
-				line := fileScanner.Text()
-				words := strings.Fields(line)
-				wordCount += len(words)
-			}
-			fmt.Printf("%d %s\n", wordCount, fileName)
-		}
-		if countChars {
-			file, err := os.Open(fileName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
-			fileScanner := bufio.NewScanner(file)
-			fileScanner.Split(bufio.ScanRunes)
-			charCount := 0
-			for fileScanner.Scan() {
-				charCount++
-			}
-			fmt.Printf("%d %s\n", charCount, fileName)
-		}
+
 	},
 }
 
@@ -97,8 +129,8 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.Flags().BoolVarP(&countBytes, "bytes", "c", false, "Count the number of bytes in the file")
-	rootCmd.Flags().BoolVarP(&countLines, "lines", "l", false, "Count the number of lines in the file")
-	rootCmd.Flags().BoolVarP(&countWords, "words", "w", false, "Count the number of words in the file")
-	rootCmd.Flags().BoolVarP(&countChars, "characters", "m", false, "Count the number of characters in the file")
+	rootCmd.Flags().BoolVarP(&options.bytes, "bytes", "c", false, "Count the number of bytes in the file")
+	rootCmd.Flags().BoolVarP(&options.lines, "lines", "l", false, "Count the number of lines in the file")
+	rootCmd.Flags().BoolVarP(&options.words, "words", "w", false, "Count the number of words in the file")
+	rootCmd.Flags().BoolVarP(&options.lines, "characters", "m", false, "Count the number of characters in the file")
 }
